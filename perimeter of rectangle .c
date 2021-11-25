97daa546d141961962e76703cfb5874e78995585
akshaya6878 <93900691+akshaya6878@users.noreply.github.com>
2021-11-25 16:32:37 +0530

    Create perimeter of rectangle .c

---
+++ perimeter of rectangle .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,a;
+    printf("enter the length of rectangle");
+    scanf("%d",&l);
+    printf("enter the breadth of rectangle");
+    scanf("%d",&b);
+    //perimeter of rectangle
+    a=l*b;
+    printf("the value of a is %d",a);
+    return 0;
+}
+
