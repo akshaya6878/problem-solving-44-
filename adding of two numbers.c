1bb72da798c86b441ed6b91d592a02f211dcaf42
akshaya6878 <93900691+akshaya6878@users.noreply.github.com>
2021-11-25 15:27:43 +0530

    Create adding of two numbers.c

---
+++ adding of two numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("enter te value o b");
+    scanf("%d",&b);
+    //adding of two numbers
+    c=a+b;
+    printf("the value of c is %d",c);
+    return 0;
+}
