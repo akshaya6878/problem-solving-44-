d50c619d0b56ee169ac511dd4cca241d54a726bf
akshaya6878 <93900691+akshaya6878@users.noreply.github.com>
2021-11-25 15:39:06 +0530

    Create temp c to f .c

---
+++ temp c to f .c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int c;
+printf("enter the value of c");
+scanf("%d",&c);
+//changing the temperature from celsius to faranheat
+float f=(1.8*c)+32;
+printf("faranheat=%f",f);
+    return 0;
+}
+
